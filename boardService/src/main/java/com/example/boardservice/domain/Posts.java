2c9ee0aee27e33b06e3c5415476515a16a323f88
WonjunS <seowj0710@gmail.com>
2023-01-25 00:53:25 +0900

    feat: create posts entity class

---
+++ boardService/src/main/java/com/example/boardservice/domain/Posts.java
@@ -0,0 +1,51 @@
+package com.example.boardservice.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Posts extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(length = 500, nullable = false)
+    private String title;
+
+    @Column(nullable = false)
+    private String content;
+
+    @Column(nullable = false)
+    private String writer;
+
+    @Column(nullable = false)
+    private int view;
+
+    @Column(nullable = false)
+    private int likes;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private BoardType boardType;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @OneToMany(mappedBy = "posts", fetch = FetchType.LAZY)
+    private List<Comment> comments;
+
+    public void update(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+
+}
