de0e2a02cf7a206d8cba5abe7ebba26959f14c4e
WonjunS <seowj0710@gmail.com>
2023-01-25 00:53:40 +0900

    feat: create comment entity class

---
+++ boardService/src/main/java/com/example/boardservice/domain/Comment.java
@@ -0,0 +1,34 @@
+package com.example.boardservice.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Comment extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String comment;
+
+    @ManyToOne
+    @JoinColumn(name = "posts_id")
+    private Posts posts;
+
+    @ManyToOne
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    public void update(String comment) {
+        this.comment = comment;
+    }
+
+}
