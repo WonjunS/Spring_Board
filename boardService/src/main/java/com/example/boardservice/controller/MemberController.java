5ed4fe76a9804b194c6c3972e3c7df172227097e
WonjunS <seowj0710@gmail.com>
2023-02-04 22:32:06 +0900

    refactor: refactor member controller class

--- boardService/src/main/java/com/example/boardservice/controller/MemberController.java
+++ boardService/src/main/java/com/example/boardservice/controller/MemberController.java
@@ -1,10 +1,9 @@
 package com.example.boardservice.controller;
 
-import com.example.boardservice.dto.MemberDto;
+import com.example.boardservice.dto.request.MemberRequestDto;
 import com.example.boardservice.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -17,18 +16,17 @@ import javax.validation.Valid;
 @RequiredArgsConstructor
 public class MemberController {
 
-    /// TODO: https://daegom.com/main/spring-post11/
     @Autowired
     private MemberService memberService;
 
     @GetMapping("/signup")
-    public String signupPage(MemberDto memberDto, Model model) {
+    public String signupPage(MemberRequestDto memberDto, Model model) {
         model.addAttribute("memberDto", memberDto);
         return "member/joinForm";
     }
 
     @PostMapping("/signup")
-    public String signup(@Valid MemberDto memberDto, BindingResult result) {
+    public String signup(@Valid MemberRequestDto memberDto, BindingResult result) {
         if(result.hasErrors()) {
             return "member/joinForm";
         }
@@ -38,19 +36,6 @@ public class MemberController {
             return "member/joinForm";
         }
         return "redirect:/";
-    }
-
-    @RequestMapping(value = "/emailCheck", method = RequestMethod.POST)
-    @ResponseBody
-    public int checkEmailDuplicate(@RequestParam("email") String email) {
-        boolean isExist = memberService.validateDuplicatedEmail(email);
-        if(isExist) return 1;
-        return 0;
-    }
-
-    @GetMapping("/user-nickname/{nickname}/exists")
-    public ResponseEntity<Boolean> checkNicknameDuplicate(@PathVariable String nickname) {
-        return ResponseEntity.ok(memberService.validateDuplicatedNickname(nickname));
     }
 
     @GetMapping("/login")
