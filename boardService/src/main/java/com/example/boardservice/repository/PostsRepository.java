18730328d53fc1d7c05a7ad72fa93c870a4931b7
WonjunS <seowj0710@gmail.com>
2023-02-04 21:15:01 +0900

    refactor: refactor posts repository class

--- boardService/src/main/java/com/example/boardservice/repository/PostsRepository.java
+++ boardService/src/main/java/com/example/boardservice/repository/PostsRepository.java
@@ -1,10 +1,24 @@
 package com.example.boardservice.repository;
 
 import com.example.boardservice.domain.Posts;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Repository
 public interface PostsRepository extends JpaRepository<Posts, Long> {
+
+    @Transactional
+    @Modifying
+    @Query("update Posts p set p.view = p.view + 1 where p.id = :id")
+    int updateView(Long id);
+
+    List<Posts> findByWriter(String writer);
 
 }
