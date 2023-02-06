73607691d54cfe5048a6769fe13e49f0895b3852
WonjunS <seowj0710@gmail.com>
2023-02-07 01:09:54 +0900

    fix: resolve datetime not saved into db error

--- boardService/src/main/java/com/example/boardservice/domain/BaseEntity.java
+++ boardService/src/main/java/com/example/boardservice/domain/BaseEntity.java
@@ -1,15 +1,18 @@
 package com.example.boardservice.domain;
 
 import lombok.Getter;
+import lombok.Setter;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.EntityListeners;
 import javax.persistence.MappedSuperclass;
+import javax.persistence.PrePersist;
+import javax.persistence.PreUpdate;
 import java.time.LocalDateTime;
 
-@Getter
+@Getter @Setter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity {
@@ -19,4 +22,16 @@ public abstract class BaseEntity {
 
     @LastModifiedDate
     private LocalDateTime modifiedAt;
+
+    @PrePersist
+    public void onPrePersist() {
+        this.createdAt = LocalDateTime.now();
+        this.modifiedAt = this.createdAt;
+    }
+
+    @PreUpdate
+    public void onPreUpdate() {
+        this.modifiedAt = LocalDateTime.now();
+    }
+
 }
