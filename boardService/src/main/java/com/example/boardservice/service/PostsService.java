b99c9f1863ecf4bd58ed79d9342a09b0554b7a87
WonjunS <seowj0710@gmail.com>
2023-02-10 16:45:42 +0900

    feat: add search and filter posts by board type methods

--- boardService/src/main/java/com/example/boardservice/service/PostsService.java
+++ boardService/src/main/java/com/example/boardservice/service/PostsService.java
@@ -4,7 +4,6 @@ import com.example.boardservice.domain.BoardType;
 import com.example.boardservice.domain.Member;
 import com.example.boardservice.domain.Posts;
 import com.example.boardservice.dto.request.PostsRequestDto;
-import com.example.boardservice.dto.response.MemberResponseDto;
 import com.example.boardservice.dto.response.PostsResponseDto;
 import com.example.boardservice.repository.MemberRepository;
 import com.example.boardservice.repository.PostsRepository;
@@ -18,7 +17,8 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import javax.persistence.EntityNotFoundException;
-import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -31,20 +31,31 @@ public class PostsService {
     private final PostsRepository postsRepository;
 
     // 게시글 올리기
-    public Long post(PostsRequestDto postsDto, String email) throws Exception {
+    public Long post(PostsRequestDto postsDto, String boardType, String email) throws Exception {
         Member member = memberRepository.findByEmail(email);
 
         if(member == null) {
             throw new EntityNotFoundException("User not found.");
         }
 
-        postsDto.setBoardType(BoardType.FREE);
+        if(boardType.equals("자유게시판")) postsDto.setBoardType(BoardType.FREE);
+        if(boardType.equals("공지사항")) postsDto.setBoardType(BoardType.NOTICE);
+        if(boardType.equals("질문게시판")) postsDto.setBoardType(BoardType.QUESTION);
+
         postsDto.setMember(member);
 
         Posts posts = postsDto.toEntity();
         postsRepository.save(posts);
 
         return posts.getId();
+    }
+
+    // 게시물 1개 불러오기
+    public PostsResponseDto getPost(Long postsId) {
+        Posts post = postsRepository.findById(postsId)
+                .orElseThrow(EntityNotFoundException::new);
+
+        return new PostsResponseDto(post);
     }
 
     // 게시물 전체 불러오기
@@ -73,18 +84,56 @@ public class PostsService {
         return posts;
     }
 
-    // 특정 게시물 불러오기
-    public PostsResponseDto getPost(Long postsId) {
-        Posts post = postsRepository.findById(postsId)
-                .orElseThrow(EntityNotFoundException::new);
+    // 검색한 게시물 불러오기
+    @Transactional(readOnly = true)
+    public Page<PostsResponseDto> searchPosts(String keyword, Pageable pageable) {
+        int page = (pageable.getPageNumber() == 0) ? 0 : (pageable.getPageNumber() - 1);
+        pageable = PageRequest.of(page, 10, Sort.by("id").descending());
 
-        return new PostsResponseDto(post);
+        Page<PostsResponseDto> posts = postsRepository.findByTitleContaining(keyword, pageable).map(PostsResponseDto::new);
+        return posts;
+    }
+
+    @Transactional(readOnly = true)
+    public Page<PostsResponseDto> getAllFreePosts(Pageable pageable) {
+        int page = (pageable.getPageNumber() == 0) ? 0 : (pageable.getPageNumber() - 1);
+        pageable = PageRequest.of(page, 10, Sort.by("id").descending());
+
+        Page<PostsResponseDto> posts =
+                postsRepository.findPostsByBoardType(BoardType.FREE, pageable).map(PostsResponseDto::new);
+
+        return posts;
+    }
+
+    public Object getAllQuestionPosts(Pageable pageable) {
+        int page = (pageable.getPageNumber() == 0) ? 0 : (pageable.getPageNumber() - 1);
+        pageable = PageRequest.of(page, 10, Sort.by("id").descending());
+
+        Page<PostsResponseDto> posts =
+                postsRepository.findPostsByBoardType(BoardType.QUESTION, pageable).map(PostsResponseDto::new);
+
+        return posts;
+    }
+
+    public Object getAllNoticePosts(Pageable pageable) {
+        int page = (pageable.getPageNumber() == 0) ? 0 : (pageable.getPageNumber() - 1);
+        pageable = PageRequest.of(page, 10, Sort.by("id").descending());
+
+        Page<PostsResponseDto> posts =
+                postsRepository.findPostsByBoardType(BoardType.NOTICE, pageable).map(PostsResponseDto::new);
+
+        return posts;
     }
 
     // 게시물 조회수 업데이트
     @Transactional
     public int updateView(Long postsId) {
         return postsRepository.updateView(postsId);
+    }
+
+    @Transactional
+    public int updateLikes(Long postsId) {
+        return postsRepository.updateLikes(postsId);
     }
 
 }
