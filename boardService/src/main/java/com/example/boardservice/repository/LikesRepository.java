a64c0ac59b16687dd8fe6f202317731ef423d4fd
WonjunS <seowj0710@gmail.com>
2023-03-05 16:11:14 +0900

    feat: create queries for like function

--- boardService/src/main/java/com/example/boardservice/repository/LikesRepository.java
+++ boardService/src/main/java/com/example/boardservice/repository/LikesRepository.java
@@ -4,14 +4,18 @@ import com.example.boardservice.domain.Likes;
 import com.example.boardservice.domain.Member;
 import com.example.boardservice.domain.Posts;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-
-import java.util.List;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface LikesRepository extends JpaRepository<Likes, Long> {
 
-    List<Likes> findByMemberAndPosts(Member member, Posts posts);
+    @Transactional
+    @Query("select l from Likes l where l.member = :member and l.posts = :posts")
+    Likes findByMemberAndPosts(Member member, Posts posts);
+
+    boolean existsLikesByMemberAndPosts(Member member, Posts posts);
 
     void deleteAllByMember(Member member);
     void deleteAllByPosts(Posts posts);
