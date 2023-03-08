b7653896ab711e30d9fcf43c27703fa351f8dcae
WonjunS <seowj0710@gmail.com>
2023-03-08 23:28:07 +0900

    feat: update activity score when the user write a comment

--- boardService/src/main/java/com/example/boardservice/controller/CommentController.java
+++ boardService/src/main/java/com/example/boardservice/controller/CommentController.java
@@ -2,6 +2,7 @@ package com.example.boardservice.controller;
 
 import com.example.boardservice.dto.request.CommentRequestDto;
 import com.example.boardservice.service.CommentService;
+import com.example.boardservice.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
@@ -14,12 +15,14 @@ import java.security.Principal;
 @RequiredArgsConstructor
 public class CommentController {
 
-    @Autowired
-    private CommentService commentService;
+    @Autowired private CommentService commentService;
+    @Autowired private MemberService memberService;
 
+    // 게시물 내 댓글 작성
     @PostMapping("/post/{postId}/write")
     public String writeComment(@PathVariable("postId") Long postId, CommentRequestDto dto, Principal principal) {
         commentService.save(postId, principal.getName(), dto);
+        memberService.updateActivityScore(principal.getName(), 5);
 
         return "redirect:/post/read/{postId}";
     }
