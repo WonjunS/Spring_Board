12fd4be9961566bf27d8d4befa2e6d172d4dfa04
WonjunS <seowj0710@gmail.com>
2023-04-09 23:31:18 +0900

    feat: update config setting

--- boardService/src/main/java/com/example/boardservice/config/SecurityConfig.java
+++ boardService/src/main/java/com/example/boardservice/config/SecurityConfig.java
@@ -1,8 +1,9 @@
 package com.example.boardservice.config;
 
-import com.example.boardservice.security.oauth.CustomOAuth2UserService;
-import com.example.boardservice.service.MemberService;
+import com.example.boardservice.security.auth.PrincipalDetailsService;
+import com.example.boardservice.security.oauth.PrincipalOAuth2UserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -18,14 +19,13 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @RequiredArgsConstructor
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final CustomOAuth2UserService customOAuth2UserService;
-
-    private final MemberService memberService;
-
     @Bean
     public PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Autowired
+    private PrincipalOAuth2UserService principalOauth2UserService;
 
     @Override
     public void configure(WebSecurity web) throws Exception {
@@ -34,24 +34,40 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(HttpSecurity http) throws Exception {
-        http.formLogin()
+//        http.formLogin()
+//                .loginPage("/member/login")
+//                .usernameParameter("email")
+//                .passwordParameter("password")
+//                .defaultSuccessUrl("/")
+//                .failureUrl("/member/login/fail")
+//                .and()
+//                .logout()
+//                .logoutRequestMatcher(new AntPathRequestMatcher("/member/logout"))
+//                .logoutSuccessUrl("/");
+//
+
+        http.csrf().disable()
+                .authorizeRequests()
+                .antMatchers("/", "/member/**", "/posts/**").permitAll()
+                .antMatchers("/admin/**").hasRole("ADMIN")
+                .and()
+                .formLogin()
                 .loginPage("/member/login")
                 .usernameParameter("email")
                 .passwordParameter("password")
                 .defaultSuccessUrl("/")
                 .failureUrl("/member/login/fail")
+                .permitAll()
                 .and()
                 .logout()
                 .logoutRequestMatcher(new AntPathRequestMatcher("/member/logout"))
-                .logoutSuccessUrl("/");
+                .logoutSuccessUrl("/")
+                .and()
+                .oauth2Login()
+                .loginPage("/member/login")
+                .userInfoEndpoint()
+                .userService(principalOauth2UserService);
 
-        http.authorizeRequests()
-                .mvcMatchers("/", "/auth/**", "/member/**","/posts/**","/images/**").permitAll()
-                .mvcMatchers("/admin/**").hasRole("ADMIN")
-                .anyRequest().authenticated();
-
-        http.exceptionHandling()
-                .authenticationEntryPoint(new CustomAuthenticationEntryPoint());
     }
 
 }
